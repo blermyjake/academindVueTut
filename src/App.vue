7afd307eabd04f4dbb1c08e2d9ad2b56a3d16090
blermyjake <jeremy73120@gmail.com>
2018-09-27 21:48:23 -0500

    push to home

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
     <hr>
     <!-- <app-message></app-message>  -->
       <router-link to="/">Home</router-link>
-      <router-link to="/users/10">Users</router-link>
+      <router-link to="/users/10">Users for team 10</router-link>
+      <router-link to="/users/12">Users for team 12</router-link>
     <hr>
     <router-view></router-view>
   </div>
--- src/Users.vue
+++ src/Users.vue
@@ -2,9 +2,26 @@
     <div>
     <h1>Users Page</h1>
     <p>Team ID is {{ $route.params.teamId }}</p>
+    <button @click="goHome">Go Home</button>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+  // lifecycle hook
+  watch: {
+    $route(to, from) {
+      //   alert(to.params.teamId);
+    }
+  },
+  created() {
+    // alert(this.$route.params.teamId);
+  },
+  methods: {
+    goHome() {
+      // this adds a push to home on the button above
+      this.$router.push("/");
+    }
+  }
+};
 </script>
